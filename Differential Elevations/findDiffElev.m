28a752ac70f0c380f2001b94d7ffd453f16dfc1a
Srijan Duggal <srijanduggal17@gmail.com>
2018-03-28 11:10:28 -0400

    Minor changes to output messages

--- Differential Elevations/findDiffElev.m
+++ Differential Elevations/findDiffElev.m
@@ -1,31 +1,31 @@
 function [sortedElevationData] = findDiffElev(elevationData, refZero)
-sprintf('It begins')
+fprintf('It begins\n')
 
 % Label each column of the CSV
 longData  = elevationData(:,1);
 latData = elevationData(:,2);
 elevData = elevationData(:,3);
-sprintf('Data Separated')
+fprintf('Data Separated\n')
 
 % Scale the dimensions of the plot to a square
 % lowestElev = min(elevData)
 lowestElev = refZero;
 highestElev = max(elevData);
-sprintf('Min and Max Elevations found')
+fprintf('Min and Max Elevations found\n')
 
 % Converts elevation points to differential points
 elevData = elevData - lowestElev;
-sprintf('Elevation transformed to differential')
+fprintf('Elevation transformed to differential\n')
 
 % Find elevation axis endpoints
 axisLow = min(elevData);
 axisHigh = max(elevData);
-sprintf('Axis Endpoints Found')
+fprintf('Axis Endpoints Found\n')
 
 % Get latitude and longitude axis values
 latVal  = unique(latData);
 longVal = unique(longData);
-sprintf('Unique latitudes and longitudes found') 
+fprintf('Unique latitudes and longitudes found\n') 
 
 % Scale axes such that left and right halves of axes are accurate
 latDiff = range(latVal);
@@ -35,18 +35,18 @@ longHalf = min(longVal) + .5 .* longDiff;
 scaleValue = max([longDiff, latDiff]);
 latDim = [(latHalf - (.5 .* scaleValue)) (latHalf + (.5 .* scaleValue))];
 longDim = [(longHalf - (.5 .* scaleValue)) (longHalf + (.5 .* scaleValue))];
-sprintf('Scale Made')
+fprintf('Scale Made\n')
 
 % Convert from degrees to meters
 averageLat = mean(latVal);
 latValMeters = 111.19.*latVal.*1000;
 longValMeters = (pi./180).*longVal.*cosd(averageLat).*6371.*1000;
-sprintf('Latitudes and longitudes converted from degrees meters') 
+fprintf('Latitudes and longitudes converted from degrees meters\n') 
 
 % Find meters from edge of region
 latValMeters = latValMeters - min(latValMeters);
 longValMeters = longValMeters - min(longValMeters);
-sprintf('Meters axis values found') 
+fprintf('Meters axis values found\n') 
 
 % Creates blank table for region
 sortedElevationData = NaN(length(latVal),length(longVal));
@@ -62,7 +62,7 @@ longinmax = max(longVal);
 % latinmax = 2.323;
 % longinmin = 33.245;
 % longinmax = 33.253;
-sprintf('Range for latitudes and longitudes found') 
+fprintf('Range for latitudes and longitudes found\n') 
 
 % t4lat = 2.3196;
 % t4long = 33.2488;
@@ -71,7 +71,7 @@ sprintf('Range for latitudes and longitudes found')
 % Create mask for subregion
 latmask = latVal >= latinmin & latVal <= latinmax;
 longmask = longVal >= longinmin & longVal <= longinmax;
-sprintf('Subregion Mask Created')
+fprintf('Subregion Mask Created\n')
 
 % Populate the table with the elevation data
 lengthLatVal = length(latVal);
@@ -84,9 +84,9 @@ for latCount = 1:lengthLatVal
         end
     end
     percentage = (latCount ./ lengthLatVal) * 100;
-    sprintf('Percentage: %.2f', percentage)
+    fprintf('Percentage: %.2f\n', percentage)
 end
-sprintf('Elevation Data Sorted')
+fprintf('Elevation Data Sorted\n')
 
 % Surface plot in degrees
 pointLableHeight = 15; %Distance from point to label
@@ -96,11 +96,11 @@ surf(longVal(longmask),latVal(latmask),sortedElevationData(latmask, longmask))
 figure(gcf)
 xlim([min(longVal(longmask)) max(longVal(longmask))])
 ylim([min(latVal(latmask)) max(latVal(latmask))])
-zlim([(axisLow) (axisHigh + pointLableHeight)])
+zlim([(axisLow) (axisHigh + pointLableHeight + 20)])
 xlabel('Longitude (deg)')
 ylabel('Latitude (deg)')
 zlabel('Elevation (meters)')
-sprintf('Figure Made')
+fprintf('Figure Made\n')
 
 % Figure settings
 % view(0,90);
@@ -115,5 +115,3 @@ plotTap('../Plots and Data/MajorPoints.xlsx', latVal, longVal, sortedElevationDa
 plotPoints('../Plots and Data/Data Files/InCountryElev.xlsx', latVal, longVal, sortedElevationData, choice, true, 'red', 'o');
 hold off
 end
-
-
